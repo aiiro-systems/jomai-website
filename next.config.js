a1c7025266114b4a21b85446533dbeb6ca74f741
UEDA Akira <ue@gol.com>
2022-09-29 20:37:45 +0900

    format

--- next.config.js
+++ next.config.js
@@ -14,8 +14,8 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-    i18n: {
-      locales: ['en', 'ja'],
-        defaultLocale: 'en',
-    },
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'en',
+  },
 });
