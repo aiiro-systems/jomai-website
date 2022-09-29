0943c77a1af9e34467d4a306dfc5b906542c543e
UEDA Akira <ue@gol.com>
2022-09-29 14:25:19 +0900

    Add contact form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -134,7 +134,7 @@ const Index = () => {
             </label>
             <input
               type="email"
-              id="email"
+              name="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
               placeholder="name@flowbite.com"
               required
@@ -148,7 +148,7 @@ const Index = () => {
               メッセージ
             </label>
             <textarea
-              id="message"
+              name="message"
               rows={6}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
               placeholder="Leave a comment..."
