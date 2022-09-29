37a878bdb420a929833fddabbeb5482f4d35020c
UEDA Akira <ue@gol.com>
2022-09-29 10:45:12 +0900

    now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
+import { AppConfig } from '@/utils/AppConfig';
 
 const Index = () => {
   const router = useRouter();
@@ -10,101 +11,84 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Jomai (ジョーマイ) アプリ"
-          description="JomaiアプリはMarkdownファイルに特化したデスクトップサーチシステムです"
+          title='Jomai (ジョーマイ) アプリ'
+          description={AppConfig.description}
         />
       }
     >
-      <h1 className="text-2xl font-bold">
+      <h1 className='text-2xl font-bold'>
         Markdownファイルを瞬時に検索
       </h1>
       <p>
-        <span role="img" aria-label="rocket">
+        <span role='img' aria-label='rocket'>
           🚀
         </span>{' '}
-          JomaiアプリはあなたのディスクにあるMarkdownドキュメントをインデックス化し検索可能にします。ひとたびインデックス化されれば、
-          Jomaiの最適化された検索機能によりドキュメントを簡単に見つけられるようになります。もうドキュメントを置く場所に悩むことはありません。
-          とにかく書いて、必要になったら検索してさっと<span role="img" aria-label="zap">
-          ⚡️
-      </span>見つけられます。
+        JomaiはあなたのコンピューターにあるMarkdownドキュメントをインデックス化し検索可能にします。ひとたびインデックス化されれば、Jomaiの最適化された検索機能によりドキュメントを簡単に見つけられるようになります。もうドキュメントを置く場所に悩むことはありません。とにかく書いて、必要になったら検索してさっと<span
+        role='img' aria-label='zap'>⚡️</span>見つけられます。
       </p>
-        <h2 className="text-lg font-semibold">Jomaiの特徴</h2>
+      <h2 className='text-lg font-semibold'>Jomaiの特徴</h2>
       <ul>
         <li>
-          <span role="img" aria-label="fire">
-            🔥
-          </span>{' '}
-            Jomai (ジョーマイ) は markdown ファイルに特化したデスクトップサーチシステムです
+          <span role='img' aria-label='rocket'>🚀</span>{' '}
+          JomaiはMarkdownファイルに特化したデスクトップサーチシステムです
         </li>
         <li>
-          <span role="img" aria-label="art">
-            🎨
-          </span>{' '}
-            ローカルディスクにあるファイルをインデックス化して検索可能にします
+          <span role='img' aria-label='pencil2'>✏️</span>{' '}
+          ローカルディスクにあるファイルをインデックス化して検索可能にします
         </li>
         <li>
-          <span role="img" aria-label="nail_care">
-            💅
-          </span>{' '}
-            ファイル名はもちろん、内容、frontmatter を使ってファイルを見つけます
+          <span role='img' aria-label='nail_care'>💅</span>{' '}
+          ファイル名はもちろん、内容、frontmatter を使ってファイルを見つけます
         </li>
         <li>
-          <span role="img" aria-label="tada">
-            🎉
-          </span>{' '}
+            <span role='img' aria-label='hammer_and_wrench'>🛠</span>{' '}
             ファイルの内容から使われている言語を推測して、その言語に最適な方法でインデックス化します (現在のバージョンでは英語と日本語に対応しています。対応言語を追加予定です)
         </li>
         <li>
-          <span role="img" aria-label="pencil2">
-            ✏️
-          </span>{' '}
-            ドキュメントをローカルディスクのどこに置いても、捕捉して検索可能にします (監視対象パスを事前に設定しておく必要があります)
+            <span role='img' aria-label='snake'>🐍</span>{' '}
+          ドキュメントをローカルディスクのどこに置いても、捕捉して検索可能にします (監視対象パスを事前に設定しておく必要があります)
         </li>
         <li>
-          <span role="img" aria-label="hammer_and_wrench">
-            🛠
-          </span>{' '}
-            プロジェクトフォルダーや他の関係のあるファイルのそばにドキュメントを置いてください。どこに置いたか覚えておく必要はありません。検索して簡単に見つけられます
+            <span role='img' aria-label='art'>🎨</span>{' '}
+          プロジェクトフォルダーや他の関係のあるファイルのそばにドキュメントを置いてください。どこに置いたか覚えておく必要はありません。検索して簡単に見つけられます
         </li>
         <li>
-          <span role="img" aria-label="fox_face">
-            🦊
-          </span>{' '}
-            見つけたファイルはお好きなエディターで開いてください
+          <span role='img' aria-label='fox_face'>🦊</span>{' '}
+          見つけたファイルはお好きなエディターで開いてください
         </li>
       </ul>
-        <h2 className="text-lg font-semibold">将来の開発予定</h2>
+      <h2 className='text-lg font-semibold'>将来の開発予定</h2>
       <ul>
         <li>
-          <span role="img" aria-label="coffee">
-            ☕
+          <span role='img' aria-label='languages'>
+              🔤
           </span>{' '}
           対応言語の追加
         </li>
         <li>
-          <span role="img" aria-label="dash">
-            💨
+          <span role='img' aria-label='documents'>
+              📑
           </span>{' '}
           Markdown以外のファイル形式に対応
         </li>
-          <li>
-              <span role="img" aria-label="white_check_mark">✅</span>{' '}
-              Windows版、Linux版の提供
-          </li>
-          <li>
-              <span role="img" aria-label="white_check_mark">
-                  ✅
+        <li>
+            <span role='img' aria-label='computer'>💻</span>{' '}
+            Windows版、Linux版の提供
+        </li>
+        <li>
+              <span role='img' aria-label='terminal'>
+                  🖥
               </span>{' '}
-              CLI(コマンドラインインターフェース)の提供
-          </li>
+          CLI(コマンドラインインターフェース)の提供
+        </li>
       </ul>
-      <h2 className="text-lg font-semibold">ダウンロード</h2>
+      <h2 className='text-lg font-semibold'>ダウンロード</h2>
       <ul>
-          <li>macOS (Appleシリコン)</li>
-          <li>macOS (Intel)</li>
+        <li>macOS (Appleシリコン)</li>
+        <li>macOS (Intel)</li>
       </ul>
-      <p className="text-sm">
-          ※ ベータ版につき30日間の使用期限があります。新しいバージョンをインストールすると期限が延長されます。
+      <p className='text-sm'>
+        ※ ベータ版につき30日間の使用期限があります。新しいバージョンをインストールすると期限が延長されます。
       </p>
     </Main>
   );
